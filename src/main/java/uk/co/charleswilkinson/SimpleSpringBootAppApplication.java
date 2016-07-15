7477e998433cca28ead3e54a18f802e3cfc879c4
Charles Wilkinson <charles.wilkinson@kedos.co.uk>
2016-07-15 16:40:12 +0100

    Imported into InsmelliJ

--- src/main/java/uk/co/charleswilkinson/SimpleSpringBootAppApplication.java
+++ src/main/java/uk/co/charleswilkinson/SimpleSpringBootAppApplication.java
@@ -1,12 +1,29 @@
 package uk.co.charleswilkinson;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SimpleSpringBootAppApplication {
 
+	private static Logger log = LogManager.getLogger();
+
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleSpringBootAppApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SimpleSpringBootAppApplication.class, args);
+
+		if(log.isDebugEnabled()) {
+			log.debug("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				log.debug(beanName);
+			}
+		}
 	}
 }
